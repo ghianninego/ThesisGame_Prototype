90bada36da83939ca6384b2d463a4a96d829c50c
Alvin CArpio <alvincarpio004@gmail.com>
2016-09-19 21:57:38 +0800

    PUSH +grass texture *main scene DESIGN ng konti

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject Player;
+	public Camera cam;
+	void Update () {
+		cam.transform.position = new Vector3 (Player.transform.position.x,Player.transform.position.y,-10);
+	}
+}
